f455e699757fc4026f098970f399c1939a1d0e01
olivier robin <olivier@ormaa.fr>
2022-09-14 21:31:15 +0200

    XCode App - project creation

---
+++ BigBurger/BigBurger/BigBurgerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BigBurgerApp.swift
+//  BigBurger
+//
+//  Created by olivier robin on 14/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct BigBurgerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
